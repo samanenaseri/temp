6706851ee3b2db57d7320c7c77cbbdbe49147a12
Samaneh Naseri <57658551+samanenaseri@users.noreply.github.com>
2025-05-18 11:05:58 +0330

    manifest

---
+++ packages/blue_thermal_printer/example/payment_printer/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.payment_printer
+
+import io.flutter.embedding.android.FlutterFragmentActivity
+
+class MainActivity : FlutterFragmentActivity() {
+}
--- packages/blue_thermal_printer/example/test/widget_test.dart
+++ packages/blue_thermal_printer/example/test/widget_test.dart
@@ -1,3 +1,2 @@
 import 'package:payment_printer/main.dart';
 
-import 'package:blue_thermal_printer/example/test/widget_test.dart'; 
